e6cba64d3930d9098640e92c27f9aae11150286f
TatsukiIshijima <tatsuki445@gmail.com>
2024-08-15 22:42:34 +0900

    add

---
+++ ROS2UnityRoboticsSimulator/Assets/Scripts/Robotics/Simulator/Production/DemoCar/Param/AxleInfo.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Robotics.Simulator.Production.DemoCar.Param
+{
+    [System.Serializable]
+    public class AxleInfo
+    {
+        public WheelCollider leftWheel;
+        public WheelCollider rightWheel;
+        public bool motor;
+        public bool steering;
+    }
+}

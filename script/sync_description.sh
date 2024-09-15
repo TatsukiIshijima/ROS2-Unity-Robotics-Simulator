4e460b90d7946057b259d187d2c8293023efba14
TatsukiIshijima <tatsuki445@gmail.com>
2024-09-15 11:26:23 +0900

    extract and copy urdf file

--- script/sync_description.sh
+++ script/sync_description.sh
@@ -6,6 +6,7 @@ ROS2_WS_SRC_DIR="ros2_docker/colcon_ws/src"
 DEST_DIR="ROS2UnityRoboticsSimulator/Assets/Urdf"
 
 # ROS2 URDF model packages
+# if you want to add more packages, add package name to PACKAGES_DIR_NAMES
 PACKAGES_DIR_NAMES=(
     "jetsonmouse_description"
 )
@@ -21,7 +22,8 @@ for package in "${PACKAGES_DIR_NAMES[@]}"; do
         echo "sync $source_dir to $DEST_DIR"
         # sync ros2 packages to Unity URDF directory
         rsync -av "$source_dir" "$DEST_DIR/"
-        # copy urdf files to Unity URDF directory"
+        # extract urdf files from subdirectories
+        find "$DEST_DIR/$package" -type f -name "*.urdf" -exec rsync -av {} "$DEST_DIR/" \;
     else
         echo "sync error: $source_dir is not found."
     fi

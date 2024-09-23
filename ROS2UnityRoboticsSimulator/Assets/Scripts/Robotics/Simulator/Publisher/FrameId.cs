8ee824ad1d513a27cdfebaee626ad3dd45289280
TatsukiIshijima <tatsuki445@gmail.com>
2024-09-23 14:09:21 +0900

    create const values

---
+++ ROS2UnityRoboticsSimulator/Assets/Scripts/Robotics/Simulator/Publisher/FrameId.cs
@@ -0,0 +1,8 @@
+namespace Robotics.Simulator.Publisher
+{
+    public static class FrameId
+    {
+        public const string RgbCameraData = "rgb_camera_data";
+        public const string InfraredRangeData = "infrared_range_data";
+    }
+}

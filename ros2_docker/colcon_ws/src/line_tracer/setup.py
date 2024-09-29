36e85e5fe2e11a26231aff6da910f5d03d9d046a
TatsukiIshijima <tatsuki445@gmail.com>
2024-09-29 20:12:30 +0900

    rename

--- ros2_docker/colcon_ws/src/line_tracer/setup.py
+++ ros2_docker/colcon_ws/src/line_tracer/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         "console_scripts": [
-            'line_tracer = line_tracer.subscriber:main'
+            'line_tracer = line_tracer.image_subscriber:main'
         ],
     },
 )

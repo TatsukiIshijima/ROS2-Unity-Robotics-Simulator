85a44c90cfd0606292d3fe579c95a367f35ed36d
TatsukiIshijima <tatsuki445@gmail.com>
2024-10-05 17:52:58 +0900

    refactor

--- ros2_docker/colcon_ws/src/line_tracer/line_tracer/image_subscriber.py
+++ ros2_docker/colcon_ws/src/line_tracer/line_tracer/image_subscriber.py
@@ -1,50 +1,26 @@
-import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge
-import cv2
-
-from line_tracer.processor import Processor
-
-"""
-ros2 run line_tracer line_tracer
-"""
 
 
 class ImageSubscriber(Node):
 
-    def __init__(self):
-        super().__init__('line_tracker')
+    def __init__(self, callback=None):
+        super().__init__('image_subscriber')
         self.subscription = self.create_subscription(
-            CompressedImage,
-            '/camera/rgb/image/compressed',
-            self.listener_callback,
-            10
+            msg_type=CompressedImage,
+            topic='/camera/rgb/image/compressed',
+            callback=self._image_callback,
+            qos_profile=10
         )
         self.bridge = CvBridge()
+        self.callback = callback
 
-        self.processor = Processor()
-
-    def listener_callback(self, msg):
+    def _image_callback(self, msg):
         # self.get_logger().info('Image Received')
-        frame = self.bridge.compressed_imgmsg_to_cv2(msg)
-        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
-        # cv2.imshow('RgbCameraImage', frame)
-        # cv2.waitKey(1)
-        self.processor.process(frame)
-        self.processor.show()
-
-
-def main(args=None):
-    rclpy.init(args=args)
-    image_subscriber = ImageSubscriber()
-    try:
-        rclpy.spin(image_subscriber)
-    except KeyboardInterrupt:
-        pass
-    image_subscriber.destroy_node()
-    rclpy.shutdown()
-
-
-if __name__ == '__main__':
-    main()
+        try:
+            frame = self.bridge.compressed_imgmsg_to_cv2(msg)
+            if self.callback is not None:
+                self.callback(frame)
+        except Exception as e:
+            self.get_logger().error(f'Error image callback: {e}')
--- ros2_docker/colcon_ws/src/line_tracer/line_tracer/main.py
+++ ros2_docker/colcon_ws/src/line_tracer/line_tracer/main.py
@@ -1,8 +1,33 @@
+import cv2
 import rclpy
-from rclpy.node import Node
+
+from line_tracer.line_tracer import LineTracer
+from line_tracer.image_subscriber import ImageSubscriber
+
+"""
+ros2 run line_tracer line_tracer
+"""
+
+
+def process_image(frame):
+    line_tracker = LineTracer()
+    frame, moment_cx, moment_cy = line_tracker.process(frame)
+    cv2.imshow('LineTracer', frame)
+    cv2.waitKey(1)
+
 
 def main(args=None):
     rclpy.init(args=args)
+    image_subscriber = ImageSubscriber(callback=process_image)
+
+    try:
+        rclpy.spin(image_subscriber)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        image_subscriber.destroy_node()
+        rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()

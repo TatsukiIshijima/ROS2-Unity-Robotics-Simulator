7fc3118840d4c953b3fbf57617ba4871b7694f6f
TatsukiIshijima <tatsuki445@gmail.com>
2026-01-01 12:06:04 +0900

    wip: create raycast lidar scan data frame id
    - temporarily using the name “laser” to visualize Lidar in Rviz2

--- ROS2UnityRoboticsSimulator/Assets/Scripts/Robotics/Simulator/Publisher/FrameId.cs
+++ ROS2UnityRoboticsSimulator/Assets/Scripts/Robotics/Simulator/Publisher/FrameId.cs
@@ -4,5 +4,7 @@ namespace Robotics.Simulator.Publisher
     {
         public const string RgbCameraData = "rgb_camera_data";
         public const string InfraredRangeData = "infrared_range_data";
+        // public const string RaycastLidarScanData = "raycast_lidar_scan_data";
+        public const string RaycastLidarScanData = "laser";
     }
 }
